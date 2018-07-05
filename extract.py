3558a3c12502830f42cebd40b1e9f52d8bbbc284
Lo Yu-Cheng <david855033@gmail.com>
2018-07-05 23:29:15 +0800

    refactor: extract
    1. remake extract.py, move function "extract" into module, in order to be called in the future
    2. add arg: extract_ratio, indicating that
     how many frames should be read/skipped before capture one frame img

--- extract.py
+++ extract.py
@@ -1,34 +1,22 @@
 from argparse import ArgumentParser
-import numpy as np
-import cv2
-import os
+from extract_module import extract
 
 parser = ArgumentParser()
 parser.add_argument("video_path", help="path of video to be extracted")
 parser.add_argument("-f", help="path of destiny folder",
                     default=".\dest", dest="folder_path")
 parser.add_argument("-n", help="path of destiny folder",
-                    default=-1, dest="MAX_IMG_NUMBER", type=int)
+                    default=-1, dest="max_img_number", type=int)
+parser.add_argument(
+    "-r", help="ratio of frame to be skipped to frame to be extracted",
+    default=0, dest="extract_ratio", type=int)
+
 
 video_path = parser.parse_args().video_path
 folder_path = parser.parse_args().folder_path
-MAX_IMG_NUMBER = parser.parse_args().MAX_IMG_NUMBER
+max_img_number = parser.parse_args().max_img_number
+extract_ratio = parser.parse_args().extract_ratio
 
-if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
-
-cap = cv2.VideoCapture(video_path)
-i = 1
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret:
-        filename = folder_path+"\\%06d.jpg" % i
-        print(filename)
-        cv2.imwrite(filename, frame)
-        i += 1
-    if cv2.waitKey(1) & 0xFF == ord('q') or(
-            i > MAX_IMG_NUMBER and MAX_IMG_NUMBER > 0):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+extract(
+    video_path=video_path, folder_path=folder_path,
+    max_img_number=max_img_number, extract_ratio=extract_ratio)
---
+++ extract_module.py
@@ -0,0 +1,24 @@
+import cv2
+import os
+
+
+def extract(video_path, folder_path, max_img_number=-1, extract_ratio=0):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    cap = cv2.VideoCapture(video_path)
+    i = 1
+    count = 1
+    ret = True
+    while(ret):
+        ret, frame = cap.read()
+        if ret:
+            filename = folder_path+"\\%06d.jpg" % count
+            if extract_ratio == 0 or i % extract_ratio == 0:
+                print(filename)
+                cv2.imwrite(filename, frame)
+                count += 1
+            i += 1
+        if i > max_img_number and max_img_number > 0:
+            break
+    cap.release()
+    print("end of extraction")
