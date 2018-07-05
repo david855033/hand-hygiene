05cf4734b3722632923b352437b3a6efbc882237
Lo Yu-Cheng <david855033@gmail.com>
2018-07-05 15:18:32 +0800

    Extract first commit
    -use extract.py "filename" -f "folder_path" -m max_image_numbr to extract video

--- extract.py
+++ extract.py
@@ -1,2 +1,33 @@
-#TODO 抓影
-#stestst
+from argparse import ArgumentParser
+import numpy as np
+import cv2
+import os
+
+parser = ArgumentParser()
+parser.add_argument("video_path", help="path of video to be extracted")
+parser.add_argument("-f", help="path of destiny folder",
+                    default=".\dest", dest="folder_path")
+parser.add_argument("-n", help="path of destiny folder",
+                    default=".\dest", dest="MAX_COUNT", type=int)
+
+video_path = parser.parse_args().video_path
+folder_path = parser.parse_args().folder_path
+MAX_COUNT = parser.parse_args().MAX_COUNT
+
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+cap = cv2.VideoCapture(video_path)
+i = 1
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret:
+        filename = folder_path+"\\%06d.jpg" % i
+        print(filename)
+        cv2.imwrite(filename, frame)
+        i += 1
+    if cv2.waitKey(1) & 0xFF == ord('q') or (i >= MAX_COUNT and MAX_COUNT > 0):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
