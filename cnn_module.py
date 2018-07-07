5caadb8713ad8f02e35417617db6ab8765a82c2f
Lo Yu-Cheng <david855033@gmail.com>
2018-07-07 15:51:24 +0800

    cnn: add tensorboard

--- cnn_module.py
+++ cnn_module.py
@@ -7,6 +7,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+from keras.callbacks import TensorBoard
 from dataloader_module import loadImgs
 
 
@@ -41,7 +42,12 @@ def train(source_path=join(os.getcwd(), "preprocess")):
                   optimizer=keras.optimizers.Adadelta(),
                   metrics=['accuracy'])
 
+
+
     model.fit(x_train, y_train,
               batch_size=batch_size,
               epochs=epochs,
-              verbose=1)
+              verbose=1,
+              callbacks=[TensorBoard(log_dir='./tmp/')])
+
+# shot tensorboard=>  tensorboard --logdir=./tmp
