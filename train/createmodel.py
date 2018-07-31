172b779c53534fa22f210c5de40b8dfe97be6ccb
Lo Yu-Cheng <david855033@gmail.com>
2018-07-31 15:57:12 +0800

    use batch normalization
    reduce parameter to 260k

--- train/createmodel.py
+++ train/createmodel.py
@@ -2,7 +2,7 @@ import keras
 from keras.applications.vgg19 import VGG19
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import load_model
 from tensorflow.python.client import device_lib
@@ -15,23 +15,31 @@ def create_model():
 
     base_model = VGG19(weights='imagenet', include_top=False,
                        input_shape=input_shape)
-    for layer in base_model.layers[:13]:
+    for layer in base_model.layers[:8]:
         layer.trainable = False
     model = Model(inputs=base_model.input,
-                  outputs=base_model.get_layer('block3_pool').output)
+                  outputs=base_model.get_layer('block2_pool').output)
     x = model.output
+    x = BatchNormalization()(x)
     x = Conv2D(filters=128, kernel_size=(1, 1),
-               activation='relu', padding='same')(x)
+               activation='relu', padding='same',
+               use_bias=False)(x)
     x = Conv2D(filters=128, kernel_size=(3, 3),
                activation='relu', padding='same')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = BatchNormalization()(x)
     x = Conv2D(filters=64, kernel_size=(1, 1),
-               activation='relu', padding='same')(x)
+               activation='relu', padding='same',
+               use_bias=False)(x)
     x = Conv2D(filters=64, kernel_size=(3, 3),
                activation='relu', padding='same')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = BatchNormalization()(x)
     x = Conv2D(filters=32, kernel_size=(1, 1),
-               activation='relu', padding='same')(x)
+               activation='relu', padding='same',
+               use_bias=False)(x)
     x = Flatten()(x)
     x = Dense(32, activation='relu')(x)
     x = Dropout(0.5)(x)
@@ -39,7 +47,7 @@ def create_model():
     model = Model(input=model.input, output=x)
 
     model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=keras.optimizers.Adadelta(),
+                  optimizer=keras.optimizers.Adam(),
                   metrics=['accuracy'])
     keras.utils.print_summary(model)
     return model
