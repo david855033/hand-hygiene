d3e429423406993252cd932eea8a651dbda05b0c
Lo Yu-Cheng <david855033@gmail.com>
2018-09-19 18:12:42 +0800

    socketio + webcam streaming

--- server/server.py
+++ server/server.py
@@ -1,22 +1,9 @@
 from flask import Flask, render_template, Response
 from flask_socketio import SocketIO, send, emit
 import cv2
-
-
-class VideoCamera(object):
-    def __init__(self):
-        # 通過opencv獲取實時視頻流
-        self.video = cv2.VideoCapture(0)
-
-    def __del__(self):
-        self.video.release()
-
-    def get_frame(self):
-        success, image = self.video.read()
-        # 因為opencv讀取的圖片並非jpeg格式，因此要用motion JPEG模式需要先將圖片轉碼成jpg格式圖片
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
-
+import threading
+import base64
+from video import VideoGet, VideoShow
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -25,23 +12,8 @@ socketio = SocketIO(app)
 
 @app.route('/')  # 主頁
 def index():
-    # jinja2模板，具體格式保存在index.html文檔中
+        # jinja2模板，具體格式保存在yemplates/index.html文檔中
     return render_template('index.html')
-
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        # 使用generator函數輸出視頻流， 每次請求輸出的content類型是image/jpeg
-        print(1)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-
-@app.route('/video_feed')  # 這個地址返回視頻流響應
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @socketio.on('message')
@@ -49,5 +21,41 @@ def handle_message(message):
     print('received message: ' + message)
 
 
+camera = VideoGet(1)
+camera.start()
+
+# video_shower = VideoShow(camera.frame).start()
+
+
+def job():
+    while True:
+        try:
+            frame = camera.frame
+            # video_shower.frame = frame
+            success, img_encoded = cv2.imencode('.jpg', frame)
+            with app.app_context():
+                socketio.emit('image', base64.b64encode(img_encoded).decode())
+            socketio.sleep(0.1)
+        except Exception as e:
+            print(e)
+            socketio.sleep(2)
+
+    # while True:
+    #     ret, frame = webcam.read()
+    #     _, img_encoded = cv2.imencode('.jpg', frame)
+    #     # with app.app_context():
+    #     #     socketio.emit('image', base64.b64encode(img_encoded).decode())
+    #     cv2.imshow('window', frame)
+    #     socketio.sleep(0.5)
+
+    # webcam.release()
+    # cv2.destroyAllWindows()
+
+
+t = threading.Thread(target=job)
+t.setDaemon(True)
+t.start()
+
+
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', debug=True, port=5000)
+    socketio.run(app, host='127.0.0.1', port=5000)
